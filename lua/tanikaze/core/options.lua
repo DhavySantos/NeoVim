fd8ec569f5238f220c71fb9b21b50c59df6517c5
dhavylts <dhavy.s@proton.me>
2024-08-07 11:28:49 -0300

    init: nvim settings

---
+++ lua/tanikaze/core/options.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = " ";
+
+vim.opt.termguicolors = true;
+vim.opt.splitright = true;
+vim.opt.splitbelow = true;
+vim.opt.swapfile = false;
+vim.opt.number = true;
+vim.opt.wrap = false;
+
+vim.opt.numberwidth = 2;
+vim.opt.shiftwidth = 2;
+vim.opt.pumheight = 10;
+vim.opt.cmdheight = 0;
+vim.opt.tabstop = 2;
+
