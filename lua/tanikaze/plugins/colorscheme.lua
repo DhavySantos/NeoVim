31d299b290ce8dd89555b91d7b925676d8fa2868
dhavylts <dhavy.s@proton.me>
2024-08-07 11:29:22 -0300

    init: colorschemes plugin

---
+++ lua/tanikaze/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+	{ "catppuccin/nvim",          name = "catppuccin", priority = 1000, config = true, opts = {} },
+	{ "folke/tokyonight.nvim",    name = "tokyonight", priority = 1000, config = true, opts = {} },
+	{ "ellisonleao/gruvbox.nvim", name = "gruvbox",    priority = 1000, config = true, opts = {} },
+	{
+		"vague2k/huez.nvim",
+		opts = {
+			exclude = {
+				"desert", "evening", "industry", "koehler", "morning", "murphy", "pablo", "peachpuff", "ron", "shine", "slate",
+				"torte", "zellner", "blue", "darkblue", "delek", "quiet", "elflord", "habamax", "lunaperche", "zaibatsu",
+				"wildcharm", "sorbet", "vim", "retrobox", "default" },
+		},
+	}
+}
