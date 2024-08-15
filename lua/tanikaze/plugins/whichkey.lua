dfea5cb7fd355ec4194e00912b9964328cad1305
Dhavy Santos <dhavy.s@proton.me>
2024-08-15 15:27:17 -0300

    feat: add toggleterm keymap

--- lua/tanikaze/plugins/whichkey.lua
+++ lua/tanikaze/plugins/whichkey.lua
@@ -3,23 +3,25 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("which-key").add({
-			{ "<leader>e",  "<cmd> Neotree focus <cr>",        desc = "Neotree Focus",              mode = { "n" } },
+			{ "<leader>e",  "<cmd> Neotree focus <cr>",                           desc = "Neotree Focus",              mode = { "n" } },
 
-			{ "<C-e>",      "<cmd> BufferNext <cr>",           desc = "Go To Next Buffer",          mode = { "n" } },
-			{ "<C-x>",      "<cmd> BufferClose <cr>",          desc = "Close Focused Buffer",       mode = { "n" } },
-			{ "<C-q>",      "<cmd> BufferPrevious <cr>",       desc = "Go To Previous Buffer",      mode = { "n" } },
-			{ "<C-X>",      "<cmd> BufferClose! <cr>",         desc = "Forse Close Focused Buffer", mode = { "n" } },
+			{ "<C-e>",      "<cmd> BufferNext <cr>",                              desc = "Go To Next Buffer",          mode = { "n" } },
+			{ "<C-x>",      "<cmd> BufferClose <cr>",                             desc = "Close Focused Buffer",       mode = { "n" } },
+			{ "<C-q>",      "<cmd> BufferPrevious <cr>",                          desc = "Go To Previous Buffer",      mode = { "n" } },
+			{ "<C-X>",      "<cmd> BufferClose! <cr>",                            desc = "Forse Close Focused Buffer", mode = { "n" } },
 
-			{ "<leader>fc", "<CMD> Huez <CR>",                 desc = "Select Colorscheme",         mode = { "n" } },
-			{ "<leader>fg", "<CMD> Telescope live_grep <CR>",  desc = "Live Grep",                  mode = { "n" } },
-			{ "<leader>fk", "<CMD> Telescope keymaps <CR>",    desc = "List Keymaps",               mode = { "n" } },
-			{ "<leader>ff", "<CMD> Telescope find_files <CR>", desc = "Find Files",                 mode = { "n" } },
-			{ "<leader>f:", "<CMD> Telescope commands <CR>",   desc = "List Commands",              mode = { "n" } },
+			{ "<leader>fc", "<CMD> Huez <CR>",                                    desc = "Select Colorscheme",         mode = { "n" } },
+			{ "<leader>fg", "<CMD> Telescope live_grep <CR>",                     desc = "Live Grep",                  mode = { "n" } },
+			{ "<leader>fk", "<CMD> Telescope keymaps <CR>",                       desc = "List Keymaps",               mode = { "n" } },
+			{ "<leader>ff", "<CMD> Telescope find_files <CR>",                    desc = "Find Files",                 mode = { "n" } },
+			{ "<leader>f:", "<CMD> Telescope commands <CR>",                      desc = "List Commands",              mode = { "n" } },
 
-			{ "<C-w>",      "<cmd> SmartCursorMoveUp <cr>",    desc = "Move Focus to Above Window", mode = { "n", "t" } },
-			{ "<C-a>",      "<cmd> SmartCursorMoveLeft <cr>",  desc = "Move Focus to Left Window",  mode = { "n", "t" } },
-			{ "<C-s>",      "<cmd> SmartCursorMoveDown <cr>",  desc = "Move Focus to Down Window",  mode = { "n", "t" } },
-			{ "<C-d>",      "<cmd> SmartCursorMoveRight <cr>", desc = "Move Focus to Right Window", mode = { "n", "t" } },
+			{ "<C-w>",      "<cmd> SmartCursorMoveUp <cr>",                       desc = "Move Focus to Above Window", mode = { "n", "t" } },
+			{ "<C-a>",      "<cmd> SmartCursorMoveLeft <cr>",                     desc = "Move Focus to Left Window",  mode = { "n", "t" } },
+			{ "<C-s>",      "<cmd> SmartCursorMoveDown <cr>",                     desc = "Move Focus to Down Window",  mode = { "n", "t" } },
+			{ "<C-d>",      "<cmd> SmartCursorMoveRight <cr>",                    desc = "Move Focus to Right Window", mode = { "n", "t" } },
+
+			{ "<C-'>",      "<cmd> ToggleTerm size=15 direction=horizontal <cr>", desc = "Toggle Horizontal Terminal", modes = { "n", "t" } },
 		});
 	end
 };
