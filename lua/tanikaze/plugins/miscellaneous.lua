65eb9913235bef3b096779800b4ce5b42dadf5a0
Dhavy Santos <dhavy.s@proton.me>
2024-08-15 10:47:56 -0300

    feat: remove plugins to have own file

--- lua/tanikaze/plugins/miscellaneous.lua
+++ lua/tanikaze/plugins/miscellaneous.lua
@@ -1,19 +1,12 @@
 return {
 	{
 		"lewis6991/gitsigns.nvim",
-		opts = {
-			current_line_blame = true,
-			sign_priority = 0,
-		}
+		opts = { current_line_blame = true, sign_priority = 0 }
 	},
 	{
 		"utilyre/barbecue.nvim",
 		dependencies = { "SmiteshP/nvim-navic" },
 		config = true,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" }
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
@@ -30,16 +23,9 @@ return {
 		config = true,
 	},
 	{
-		"j-hui/fidget.nvim",
-		config = true,
-	},
-	{
 		"folke/neodev.nvim",
 		config = true,
 	},
 	{ "mrjones2014/smart-splits.nvim" },
 	{ "mg979/vim-visual-multi" },
-	{ "rcarriga/nvim-notify" },
-	{ "MunifTanjim/nui.nvim" },
-	{ "mbbill/undotree" },
 };
