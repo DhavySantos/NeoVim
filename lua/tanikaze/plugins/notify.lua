ac656ed4c6af45aa0f8d172a0945cca3865be33e
Dhavy Santos <dhavy.s@proton.me>
2024-08-15 10:48:25 -0300

    init: setup notify plugin

---
+++ lua/tanikaze/plugins/notify.lua
@@ -0,0 +1,26 @@
+return {
+	"rcarriga/nvim-notify",
+	config = function()
+		require("notify").setup({
+			background_colour = "NotifyBackground",
+			fps = 30,
+			icons = {
+				DEBUG = "",
+				ERROR = "",
+				INFO = "",
+				TRACE = "✎",
+				WARN = ""
+			},
+			level = 2,
+			minimum_width = 50,
+			render = "default",
+			stages = "fade_in_slide_out",
+			time_formats = {
+				notification = "%T",
+				notification_history = "%FT%T"
+			},
+			timeout = 1000,
+			top_down = true
+		});
+	end
+}
