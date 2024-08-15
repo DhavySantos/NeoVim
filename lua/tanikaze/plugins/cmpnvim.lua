3f11eecf757ac4a6f229684f8732e438f0df590a
Dhavy Santos <dhavy.s@proton.me>
2024-08-15 10:45:02 -0300

    feat: add css cmp plugins

--- lua/tanikaze/plugins/cmpnvim.lua
+++ lua/tanikaze/plugins/cmpnvim.lua
@@ -1,16 +1,17 @@
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
-		"L3MON4D3/LuaSnip",
 		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-vsnip",
-		"hrsh7th/vim-vsnip",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-nvim-lsp",
+		"L3MON4D3/LuaSnip",
+		"jcha0713/cmp-tw2css",
 		"onsails/lspkind.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"saadparwaiz1/cmp_luasnip",
 	},
+
 	config = function()
-		local cmp = require("cmp")
+		local cmp = require("cmp");
+
 		local function close_fallback(fallback)
 			require("cmp").close();
 			fallback();
@@ -22,37 +23,37 @@ return {
 					winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
 					col_offset = -3,
 					side_padding = 0,
-				}
+				},
 			},
 
 			formatting = {
-				fields = { "kind", "abbr" },
+				fields = { "kind", "abbr", "menu" },
 				format = function(entry, vim_item)
 					local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
 					local strings = vim.split(kind.kind, "%s", { trimempty = true })
-					kind.menu = "    (" .. (strings[2] or "") .. ")"
 					kind.kind = " " .. (strings[1] or "") .. " "
+					kind.menu = "    (" .. (strings[2] or "") .. ")"
+
 					return kind
 				end,
 			},
 
 			mapping = cmp.mapping.preset.insert({
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-				["<Tab>"] = cmp.mapping.select_next_item(),
-				["<C-e>"] = cmp.mapping.abort(),
-
+				["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+				["<Tab>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
 				["<Down>"] = cmp.mapping(close_fallback),
 				["<Up>"] = cmp.mapping(close_fallback),
-
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<C-e>"] = cmp.mapping.abort(),
 			}),
 
 			sources = cmp.config.sources({
+				{ name = "cmp-tw2css" },
 				{ name = "nvim_lsp" },
-				{ name = "buffer" },
 				{ name = "path" },
 			}),
 
+			preselect = "item",
 		});
 	end
 }
