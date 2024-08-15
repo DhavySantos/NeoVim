1ba712a3226591f8a7c5f874200da6991dad7cfc
Dhavy Santos <dhavy.s@proton.me>
2024-08-15 10:47:29 -0300

    feat: update lsp settings

--- lua/tanikaze/plugins/lspzero.lua
+++ lua/tanikaze/plugins/lspzero.lua
@@ -3,30 +3,43 @@ return {
 	dependencies = {
 		"neovim/nvim-lspconfig",
 		"williamboman/mason.nvim",
+		"lukas-reineke/lsp-format.nvim",
 		"williamboman/mason-lspconfig.nvim",
 	},
 
 	config = function()
-		local lsp = require("lsp-zero");
-
-		require("mason").setup();
-		require("mason-lspconfig").setup_handlers({ function(server)
-			require("lspconfig")[server].setup({});
-		end });
-
-		vim.diagnostic.config({
-			virtual_text = true,
-			underline = true,
-			signs = false,
+		local lsp_attach = function(client, bufnr)
+			require("lsp-format").on_attach(client, bufnr);
+			local opts = { buffer = bufnr, silent = true, noremap = true };
+			vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts);
+			vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts);
+			vim.keymap.set("n", "<F3>", "<cmd>lua vim.lsp.buf.format()<cr>", opts);
+			vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts);
+			vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts);
+			vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts);
+		end
+		require("lsp-zero").extend_lspconfig({
+			sign_text = { error = "", warn = "", hint = "", info = "" },
+			lsp_attach = lsp_attach,
 		});
 
-		local signs = { Error = "", Warn = "", Hint = "", Info = "" };
+		require("mason").setup();
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				require("lspconfig")[server_name].setup({});
+			end,
 
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-		end
-
-		lsp.setup();
+			["lua_ls"] = require("lspconfig").lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { 'vim' },
+							undefined_global = false,
+							missing_parameters = false,
+						}
+					}
+				},
+			}),
+		});
 	end
 }
