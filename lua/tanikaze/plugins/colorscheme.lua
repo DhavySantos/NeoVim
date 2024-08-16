0d4e87613fabcca968485aef0c1946a167fad142
Dhavy Santos <dhavy.s@proton.me>
2024-08-16 15:28:15 -0300

    feat: include rose-pine to colors

--- lua/tanikaze/plugins/colorscheme.lua
+++ lua/tanikaze/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
 return {
 	{ "catppuccin/nvim",          name = "catppuccin", priority = 1000, config = true, opts = {} },
 	{ "folke/tokyonight.nvim",    name = "tokyonight", priority = 1000, config = true, opts = {} },
+	{ "rose-pine/neovim",         name = "rose-pine",  priority = 1000, config = true, opts = {} },
 	{ "ellisonleao/gruvbox.nvim", name = "gruvbox",    priority = 1000, config = true, opts = {} },
 	{
+
 		"vague2k/huez.nvim",
 		opts = {
 			exclude = {
