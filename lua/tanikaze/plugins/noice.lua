d4acd0e0b3c8b5db9384d7c6bed06e572a66e751
Dhavy Santos <dhavy.s@proton.me>
2024-08-20 14:56:48 -0300

    feat: make lsp docs window be bordered

--- lua/tanikaze/plugins/noice.lua
+++ lua/tanikaze/plugins/noice.lua
@@ -6,13 +6,10 @@ return {
 	},
 	opts = {
 		lsp = {
-			progress = {
-				enable = false,
-			},
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				["cmp.entry.get_documentation"] = true,
 			},
 		},
 		-- you can enable a preset for easier configuration
@@ -21,7 +18,7 @@ return {
 			command_palette = true,    -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
 			inc_rename = false,        -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false,    -- add a border to hover docs and signature help
+			lsp_doc_border = true,    -- add a border to hover docs and signature help
 		}
 	}
 }
