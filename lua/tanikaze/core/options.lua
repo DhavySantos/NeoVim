ed389d1ee79439c92a16dc91bfae4bddbc22ba83
Dhavy Santos <dhavy.s@proton.me>
2024-09-11 23:00:49 -0300

    feat: set cmp kind_icons

--- lua/tanikaze/core/options.lua
+++ lua/tanikaze/core/options.lua
@@ -8,6 +8,35 @@ vim.g.signs = {
 	info = ""
 };
 
+vim.g.kind_icons = {
+  Text = "",
+  Method = "󰆧",
+  Function = "󰊕",
+  Constructor = "",
+  Field = "󰇽",
+  Variable = "󰂡",
+  Class = "󰠱",
+  Interface = "",
+  Module = "",
+  Property = "󰜢",
+  Unit = "",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "󰌋",
+  Snippet = "",
+  Color = "󰏘",
+  File = "󰈙",
+  Reference = "",
+  Folder = "󰉋",
+  EnumMember = "",
+  Constant = "󰏿",
+  Struct = "",
+  Event = "",
+  Operator = "󰆕",
+  TypeParameter = "󰅲",
+}
+
+
 vim.opt.termguicolors = true;
 vim.opt.splitright = true;
 vim.opt.splitbelow = true;
