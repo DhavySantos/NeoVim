0e9df5f8a0e00adb4f3a89d54dab1a81993032b4
Dhavy Santos <dhavy.s@proton.me>
2024-09-21 00:23:05 -0300

    fix: Set nvim-treesitter event to BufReadPre / BufNewFile

--- lua/tanikaze/plugins/treesitter.lua
+++ lua/tanikaze/plugins/treesitter.lua
@@ -1,9 +1,14 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
+
+	dependencies = { "windwp/nvim-ts-autotag" },
+
 	opts = {
 		auto_install = true,
 		indent = { enable = true },
+		autotag = { enable = true },
 		highlight = { enable = true },
-	}
+	},
 }
