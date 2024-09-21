d95dd1f2facaeb91161188b5d44992a4cd43df95
Dhavy Santos <dhavy.s@proton.me>
2024-09-21 00:21:32 -0300

    fix: Setup telescope keymaps with lazy instead whichkey

--- lua/tanikaze/plugins/telescope.lua
+++ lua/tanikaze/plugins/telescope.lua
@@ -1,16 +1,30 @@
 return {
-    "nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-ui-select.nvim",
+	"nvim-telescope/telescope.nvim",
+	event = "VimEnter",
+
+	keys = {
+		{ "<leader>fc", "<cmd> Telescope colorscheme <cr>", mode = { "n" } },
+		{ "<leader>fs", "<cmd> Telescope grep_string <cr>", mode = { "n" } },
+		{ "<leader>ff", "<cmd> Telescope find_files <cr>",  mode = { "n" } },
+		{ "<leader>fb", "<cmd> Telescope buffers <cr>",     mode = { "n" } },
 	},
-	config = function()
-		require("telescope").load_extension("ui-select");
 
-		require("telescope").setup({
-			pickers = { colorscheme = { enable_preview = true } },
-		});
+	dependencies = {
+		"nvim-telescope/telescope-ui-select.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"nvim-lua/plenary.nvim",
+	},
 
+	opts = {
+		pickers = {
+			colorscheme = { enable_preview = true },
+		}
+	},
+
+	config = function(_, opts)
+		local telescope = require("telescope");
+
+		telescope.load_extension("ui-select");
+		telescope.setup(opts);
 	end,
 }
-
