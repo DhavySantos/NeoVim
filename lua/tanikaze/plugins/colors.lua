9a83d3e829b0e713eb0ba9ab13ef2bc39de8447e
Dhavy Santos <dhavy.s@proton.me>
2024-10-21 22:04:06 -0300

    feat: setup SchemeKeeper plugin to handle colorscheme persistence

--- lua/tanikaze/plugins/colors.lua
+++ lua/tanikaze/plugins/colors.lua
@@ -1,7 +1,13 @@
 return {
-	{ "ellisonleao/gruvbox.nvim" },
-	{ "tiagovla/tokyodark.nvim" },
-	{ "folke/tokyonight.nvim" },
-	{ "rose-pine/neovim" },
-	{ "catppuccin/nvim" },
+  "DhavySantos/SchemeKeeper.nvim",
+
+  opts = { },
+
+  dependencies = {
+    "ellisonleao/gruvbox.nvim",
+    "tiagovla/tokyodark.nvim",
+    "folke/tokyonight.nvim",
+    "rose-pine/neovim",
+    "catppuccin/nvim",
+  }
 }
